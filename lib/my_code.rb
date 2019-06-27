97bb7349ad48f0de7d056d8499c519832907ea83
Eric Winbigler <uzof99@gmail.com>
2019-06-27 16:59:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ end
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * 1 )
+    new.push( source_array[i] )
     i += 1
   end
   return new
@@ -32,46 +32,37 @@ end
  def map_to_square(source_array)
   new = []
   i = 0
-  while i < source_array.length do 
-    new.push( source_array[i] ** 2 )
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] )
     i += 1
   end
   return new
 end
 
- def reduce_to_total(source_array)
-  source_array = [1, 2, 3]
-
-   reduce_to_total = source_array.reduce(0) { |sum, number|  sum + number }
-end
-
- def reduce_to_total(source_array, starting_point)
-  source_array = [1, 2, 3]
-  starting_point = 100
-
-   reduce_to_total = source_array.reduce(100) { |sum, number| sum + number }
+ def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
 end
 
  def reduce_to_all_true(source_array)
-  source_array = [1, 2, true, "razmatazz"]
-
-   reduce_to_all_true = source_array.reduce { |n| total = n }
-end
-
- def reduce_to_all_true(source_array)
-  source_array = [1, 2, true, "razmatazz", false]
-
-   reduce_to_all_true = source_array.reduce { |falsey| total = false}
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
  def reduce_to_any_true(source_array)
-  source_array = [false, nil, nil, nil, true]
-
-   reduce_to_any_true = source_array.reduce { |truthy| total = true}
-end
-
- def reduce_to_any_true(source_array)
-  source_array = [false, nil, nil, nil, true]
-
-   reduce_to_any_true = source_array.reduce { |falsey| total = false}
-end
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end 
