480db9fb629c3bbcbe65b48dee094961e1434222
Eric Winbigler <uzof99@gmail.com>
2019-06-27 16:52:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,5 +3,10 @@ def map_to_negativize(array)
     array.map { |i| i*-1 }
 end
 
-def map_to_change(array)
+def map_to_no_change(array)
   array.map {|i| i}
+end
+
+def map_to_double(array)
+  array.map {|i| }
+end
