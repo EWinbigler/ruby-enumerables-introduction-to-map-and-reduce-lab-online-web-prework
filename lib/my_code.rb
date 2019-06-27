228ff3a09b8a8cd563a314ad6bff808d1223f654
Eric Winbigler <uzof99@gmail.com>
2019-06-27 16:56:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,4 +36,13 @@ def reduce_to_all_true(array)
     true
 end
 
-
+def reduce_to_any_true(array)
+      i = 0
+      while i < array.length do      
+        if array[i]
+          return true
+        end
+        i += 1
+      end
+    false
+    
